2a07a8549bdecec899c9254b3dffcf7e8bd2d09a
Takashi Matsuoka <matsujirushi@live.jp>
2020-02-26 11:46:52 +0900

    Change type of set_server parameter

--- NTPClient.h
+++ NTPClient.h
@@ -22,13 +22,13 @@
 class NTPClient {
     public:
         explicit NTPClient(NetworkInterface *interface = NULL);
-        void set_server(char* server, int port);
+        void set_server(const char* server, int port);
         time_t get_timestamp(int timeout = 15000);
         void network(NetworkInterface *interface);
 
     private:
         NetworkInterface *iface;
-        char* nist_server_address;
+        const char* nist_server_address;
         int nist_server_port;
 
         uint32_t ntohl(uint32_t num);
